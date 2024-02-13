f541dba738a0d3257c048c5749bafbb0f105bca3
WillOtterbein <willjordan05@icloud.com>
2024-02-13 13:58:14 -0800

    main file

--- assignment2.c
+++ assignment2.c
@@ -1,9 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Definitions
+
+// Method headers
+void readFile();
+void simulateFunction();
+void outputFile();
+void printErrorExit();
+
+// Constants
+FILE *in;
+FILE *out; 
+
 // Main Method
-int main()
-{
+int main(int argc, char *argv[]) {
+    if (argc != 4) {
+        return 1;
+    }
+
+    // Setup files
+    readFile();
+
+    simulateFunction();
 
     return 0;
 }
