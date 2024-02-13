b5c0735aa330ea50d611965b3ead147041116ad4
lucaslav05 <lucaslaviolette05@gmail.com>
2024-02-13 13:56:11 -0800

    Added Particle Struct

--- assignment2.c
+++ assignment2.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct{
+    int posX;
+    int posY;
+    int velX;
+    int velY;
+} Particle;
+
+
 // Main Method
 int main()
 {
-
+    
     return 0;
 }
